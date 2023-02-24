89d087926cf00ad2c1627516da8e74eea86b407f
Venu Yanamandra <venu.yanamandra@live.com>
2023-02-24 10:43:06 -0800

    Create get-kv-blocking.py

---
+++ get-kv-blocking.py
@@ -0,0 +1,57 @@
+import sys
+import requests
+import argparse
+from datetime import datetime
+
+
+def dttm_this_get(url, key, index, consistent, wait, token):
+    resp = ''
+    status_code = 200
+    get_url = '{}/v1/kv/{}?index={}&wait={}'.format(url, key, index, wait)
+    if consistent:
+        get_url = '{}&consistent='.format(get_url)
+
+    start = datetime.now()
+
+    headers = None
+    if len(token) > 0:
+        headers = {'x-consul-token': token}
+
+    try:
+        r = requests.get(get_url, headers=headers, verify=False)
+        status_code = r.status_code
+        if r.ok:
+            resp = str(r.json()[0])
+        else:
+            resp = 'Errored (does the key exist?)'
+
+    except Exception as e:
+        resp = 'Exception: <{}>'.format(e)
+
+    end = datetime.now()
+
+    return '{} | {} | {} | {} | {} | {}'.format(get_url, status_code, str(start), str(end), end - start, resp)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='{}'.format(sys.argv[0]), description='Understand BlockingQueries, MaxQueryTime, DefaultQueryTime through KV query.', epilog='Venu Yanamandra thanks you for trying. :)')
+    parser.add_argument('-u', '--url', help='Consul HTTP Addr',
+                        default='http://127.0.0.1:8500')
+    key_required = parser.add_argument_group('Required arguments')
+    key_required.add_argument('-k', '--key', required=True, help='Key to retrieve')
+    parser.add_argument('-i', '--index', type=int, default=1)
+    parser.add_argument('-c', '--consistent', type=bool, default=True)
+    parser.add_argument('-w', '--wait', default='1s')
+    parser.add_argument('-t', '--token', default='')
+
+    args = parser.parse_args()
+
+    r = dttm_this_get(args.url, args.key, args.index,
+                      args.consistent, args.wait, args.token)
+
+    print(r)
+
+
+if __name__ == '__main__':
+    main()
